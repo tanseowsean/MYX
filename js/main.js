dd27b5e97d4bd1018e740deabee4a819366bce27
tanseowsean <tanseowsean@gmail.com>
2023-06-02 23:02:15 +0800

    Fixed some styling issues

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Begin JS
+
+$('.hamburger').click(function () {
+    $('.mobile-menu').addClass('active');
+});
+
+$('.mobile-menu .close').click(function () {
+    $('.mobile-menu').removeClass('active');
+});
